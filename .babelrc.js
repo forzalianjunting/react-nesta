381136d91650a75cc91eac10f55ba7a1dee272d0
lianjunting <276644176@qq.com>
2019-01-28 15:02:11 +0800

    :bug: add babel plugins

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env"
+    ]
+  ],
+  "plugins":[
+    "@babel/proposal-class-properties",
+    "@babel/proposal-object-rest-spread",
+    "@babel/transform-react-jsx"
+  ]
+}
